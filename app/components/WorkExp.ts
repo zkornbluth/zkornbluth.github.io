208673cc0a9853f3e853e2f2539e5c6813c8cfbd
zkornbluth <zkornbluth2007@gmail.com>
2025-08-06 19:15:57 -0400

    Added description for Epic experience

--- app/components/WorkExp.ts
+++ app/components/WorkExp.ts
@@ -16,7 +16,7 @@ export class WorkExp {
 
 const workExps: WorkExp[] = [
     new WorkExp("Freelance AI Consultant", "Horizon Media", "2025-present", "Automating company analysis through ChatGPT and Gemini", ["LLMs"]),
-    new WorkExp("Software Developer", "Epic Systems", "2021-2023", "Placeholder", ["React", "TypeScript", "C#", "GT.M/Cache"]),
+    new WorkExp("Software Developer", "Epic Systems", "2021-2023", "Developed new features and fixed bugs for orthopedics and rheumatology practices", ["React", "TypeScript", "C#", "GT.M/Cache"]),
     new WorkExp("Software Development Engineer Intern", "Amazon", "Summer 2020", "Designed and developed command-line package to extract Python code from Jupyter notebook and ship to review", ["Python", "Jupyter"]),
     new WorkExp("Software Development Engineer Intern", "Amazon", "Summer 2019", "Created set of APIs to act as data access layer between database and web service", ["Java", "APIs"]),
     new WorkExp("Tech Intern", "Exiger", "Summer 2018", "Conducted QA on data scraping and survey software", ["Quality Assurance"])
