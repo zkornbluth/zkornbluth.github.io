7ccfa4761c4c02be0e4ba14442dc8be8a85972cc
zkornbluth <zkornbluth2007@gmail.com>
2025-08-10 12:41:01 -0400

    Updates to projects
    New screenshot for todos, and moved typing test to the last project

--- app/components/Project.ts
+++ app/components/Project.ts
@@ -28,10 +28,10 @@ const projects: Project[] = [
     new Project("Wheel of Fortune Dashboard", ["R", "Shiny"], "Improve your Bonus Round puzzle solving", wheeloffortune, "https://github.com/zkornbluth/WheelOfFortune", "http://zkornbluth.github.io/WheelOfFortune"),
     new Project("Menu Bar Password Generator", ["Swift"], "Generate random passwords with rules", passwordgenerator, "https://github.com/zkornbluth/PasswordGenerator"),
     new Project("Job Posting Filtering Tool", ["Python", "pandas"], "Scrape Indeed & LinkedIn with filters", jobsearcher, "https://github.com/zkornbluth/job_searcher"),
-    new Project("Typing Speed Test", ["React", "TypeScript", "Next.js"], "Measure your words per minute & accuracy", typing, "https://github.com/zkornbluth/typing-test", "https://zkornbluth.github.io/typing-test"),
     new Project("To Do Manager", ["React", "TypeScript", "Next.js"], "Track your tasks with easy editing", todos, "https://github.com/zkornbluth/todo-manager", "https://zkornbluth.github.io/todo-manager"),
     new Project("File Organizer", ["Java", "JavaFX"], "Sort files into folders by extension", fileorganizer, "https://github.com/zkornbluth/FileOrganizer"),
-    new Project("Weather Widget", ["Python", "tkinter"], "View real-time weather data by location", weather, "https://github.com/zkornbluth/weather_app")
+    new Project("Weather Widget", ["Python", "tkinter"], "View real-time weather data by location", weather, "https://github.com/zkornbluth/weather_app"),
+    new Project("Typing Speed Test", ["React", "TypeScript", "Next.js"], "Measure your words per minute & accuracy", typing, "https://github.com/zkornbluth/typing-test", "https://zkornbluth.github.io/typing-test")
 ];
 
 export {projects};
