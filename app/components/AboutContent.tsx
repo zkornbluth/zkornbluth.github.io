0db51fd573e3e8859b780e640f53fb51cf29e20b
zkornbluth <zkornbluth2007@gmail.com>
2026-02-16 13:17:36 -0500

    Added Tableau certification badge to About
    Once I have more (GCP, Tableau) should make that more dynamic than just hardcoding it

--- app/components/AboutContent.tsx
+++ app/components/AboutContent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import headshot from '../assets/headshot.jpeg';
+import tableau_desktop_foundations from '../assets/tableau-desktop-foundations.png';
 import { workExps } from '../models/WorkExp';
 import DisplayWorkExp from './DisplayWorkExp';
 
@@ -24,6 +25,14 @@ export default function AboutContent() {
                 <Link className="resume-link" href="/resume.pdf" target="_blank" rel="noopener noreferrer" title="View my full resume">
                     View my resume
                 </Link>
+                <br></br>
+                <Image
+                    src={tableau_desktop_foundations}
+                    alt="Salesforce Certified Tableau Desktop Foundations"
+                    width={50}
+                    height={50}
+                    style={{marginTop: 20}}
+                />
             </div>
             <div className="workexps-container">
                 <h3>Experience</h3>
--- app/models/WorkExp.ts
+++ app/models/WorkExp.ts
@@ -7,7 +7,7 @@ interface WorkExp {
 }
 
 export const workExps: WorkExp[] = [
-    { title: "Freelance AI Consultant", company: "Horizon Media", years: "2025-present", description: "Automating company analysis through ChatGPT and Gemini", technologies: ["AI", "LLMs"] },
+    { title: "Freelance AI Consultant", company: "Horizon Media", years: "2025", description: "Automated company analysis through ChatGPT and Gemini", technologies: ["AI", "LLMs"] },
     { title: "Software Developer", company: "Epic Systems", years: "2021-2023", description: "Developed new features and fixed bugs for orthopedics and rheumatology practices", technologies: ["React", "TypeScript", "C#", "GT.M/Cache"] },
     { title: "Software Development Engineer Intern", company: "Amazon", years: "Summer 2020", description: "Designed and developed command-line package to extract Python code from Jupyter notebook and ship to review", technologies: ["Python", "Jupyter"] },
     { title: "Software Development Engineer Intern", company: "Amazon", years: "Summer 2019", description: "Created set of APIs to act as data access layer between database and web service", technologies: ["Java", "APIs"] },
